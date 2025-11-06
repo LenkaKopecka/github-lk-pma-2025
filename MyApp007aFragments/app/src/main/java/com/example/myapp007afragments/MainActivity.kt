2e473cd66d54021e618a9d1a5b041dd80919f404
lenkakopecka <lenka.kopecka@tul.cz>
2025-11-06 15:08:19 +0100

    commit

---
+++ MyApp007aFragments/app/src/main/java/com/example/myapp007afragments/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapp007afragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapp007afragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        // získání instance správce fragmentu
+
+        val fragmentManager = supportFragmentManager
+
+
+        // vytvoření nové transakce pro fragmenty
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // nahrazení fragmentu ve view kontejneru novým fragmentem, který byl předán jako argument
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+        // potvrzení transakce a provedení výměny fragmentu v
+        fragmentTransaction.commit()
+    }
+}
